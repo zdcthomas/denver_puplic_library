3658098ac18562b8b5742ea143980886d080b290
zdcthomas <zdcthomas@yahoo.com>
2018-04-11 09:22:33 -0600

    author class WIP

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,17 @@
 class Author
 
+  attr_reader :books
+
+  def initialize(author_data)
+    @author_data = author_data
+    @books = []
+  end
+
+  def add_book(title, publication_date)
+    book_data = {:title => title, 
+                 :publication_date => publication_date,
+                 :author_first_name => @author_data[author_first_name],
+                 :author_last_name => @author_data[author_last_name]}
+    @books << Book.new(book_data)
+  end
 end
