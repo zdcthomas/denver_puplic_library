799eb2d23e773bcf63711dba5997c088d046fcda
zdcthomas <zdcthomas@yahoo.com>
2018-04-11 10:07:46 -0600

    TESTS iteration5

--- test/library_test.rb
+++ test/library_test.rb
@@ -58,7 +58,38 @@ class LibraryTest < Minitest::Test
     library.add_to_collection(jane_eyre)
     expected = [villette, jane_eyre, mockingbird]
     assert_equal expected, library.card_catalogue
+  end 
 
+  def test_find_by_author
+    ibrary = Library.new
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+    library.add_to_collection(mockingbird)
+    library.add_to_collection(villette)
+    library.add_to_collection(jane_eyre)
+    actual = library.find_by_author("Charlotte Bronte")
+    expected = {
+            "Jane Eyre" => jane_eyre
+            "Villette"  => villette
+            }
+    assert_equal expected, actual
+  end
 
+  def test_find_by_author
+    ibrary = Library.new
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+    library.add_to_collection(mockingbird)
+    library.add_to_collection(villette)
+    library.add_to_collection(jane_eyre)
+    actual = library.find_by_publication_date("1960")
+    expected = {"To Kill a Mockingbird" => mockingbird}
+    assert_equal expected, actual
   end 
 end
