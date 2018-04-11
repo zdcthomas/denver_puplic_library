cde1a3bc5650cf52d867b31bbcbfed3593791271
zdcthomas <zdcthomas@yahoo.com>
2018-04-11 10:21:38 -0600

    cleans up library class

--- lib/library.rb
+++ lib/library.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 require_relative './book'
 require 'pry'
-class Library
 
+# stores books, and allows for searches on those books
+class Library
   attr_reader :books
   def initialize
     @books = []
-  end 
+  end
 
   def add_to_collection(book)
     @books << book
@@ -16,34 +19,31 @@ class Library
   end
 
   def card_catalogue
-    # binding.pry
-   @books.sort_by{|book| book.author_last_name}
-  end 
+    @books.sort_by(&:author_last_name)
+  end
 
   def find_by_author(author)
-    author_first_name = author.split(" ")[0]
-    author_last_name = author.split(" ")[1]
+    author_first_name = author.split(' ')[0]
+    author_last_name = author.split(' ')[1]
     relevant_books = @books.find_all do |book|
-       book.author_first_name == author_first_name && book.author_last_name == author_last_name
+      book.author_first_name == author_first_name && book.author_last_name == author_last_name
     end
-    found = {}
-    relevant_books.each do |book|
-      title = book.title
-      found[title] = book
-    end 
-    found
+    return_found_books(relevant_books)
   end
 
   def find_by_publication_date(publication_date)
     relevant_books = @books.find_all do |book|
       book.publication_date.include?(publication_date)
     end
+    return_found_books(relevant_books)
+  end
+
+  def return_found_books(relevant_books)
     found = {}
     relevant_books.each do |book|
       title = book.title
       found[title] = book
-    end 
+    end
     found
   end
-      
 end
